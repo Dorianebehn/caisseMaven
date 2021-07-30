beaa2b83c164461351b9d6d0051f6af039528eb6
Your Dorianebehn <dorybehngueut@gmail.com>
2021-07-30 13:21:21 +0200

    reprise des frames de shellaa

---
+++ src/main/java/com/example/caissemarven/windows/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.caissemarven.windows;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ *
+ * @author Dory
+ */
+public class Principal extends JFrame {
+   public void actionButton(ActionEvent e ){
+       String s = e.getActionCommand();
+       
+   }
+}
